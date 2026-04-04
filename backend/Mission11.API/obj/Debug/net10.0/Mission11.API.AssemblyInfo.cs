feeadd0b9129424f15ef1ec697ae0fbd93207cdd
mbwoodal <mbwoodal@byu.edu>
2026-04-03 20:39:42 -0600

    fixed backend

--- backend/Mission11.API/obj/Debug/net10.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net10.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfc34333b47cbd921d1d4c0fad7fc561e9f7a61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f283c5e6734aec650d836e10b2f51c2d332cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Mission11.API/obj/Release/net10.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Release/net10.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff48dea7dfc51712cefd4c1df87d58c88ea0493")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f283c5e6734aec650d836e10b2f51c2d332cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
